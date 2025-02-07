d42f9f1cb55ae72919c5f97e6cb7de9ebaa04e33
IvanCardoso91 <ivancardoso1991@gmail.com>
2025-02-07 19:06:52 -0300

    initial commit: project configuration OK

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind CSS está funcionando! 🎉</h1>
+      <button className="mt-6 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition-all">
+        Teste de botão
+      </button>
+    </div>
+  );
+}
